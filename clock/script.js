c84ee1badc5a9e832c546b02f2e7a834dde6a93b
ImNotACat6 <40551723+ImNotACat6@users.noreply.github.com>
2023-08-12 19:28:11 -0400

    fixed widget shadow and sizing

--- clock/script.js
+++ clock/script.js
@@ -62,7 +62,7 @@ function Time() {
   second = update(second);
  
   // Adding time elements to the div
-  document.getElementById("clock_text").innerText = hour + " : " + minute + " : " + second + " " + period;
+  document.getElementById("clock").innerText = hour + " : " + minute + " : " + second + " " + period;
  
   // Set Timer to 1 sec (1000 ms)
   setTimeout(Time, 1000);
