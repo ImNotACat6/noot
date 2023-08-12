79d17468298d7337aa0b667cbe736e2866f1d29f
ImNotACat6 <40551723+ImNotACat6@users.noreply.github.com>
2023-08-12 11:25:58 -0400

    Fix CSS

--- clock/script.js
+++ clock/script.js
@@ -1,28 +1,5 @@
 
 // Dynamic theme based on OS preferences
-function light() {
-	document.documentElement.setAttribute('theme', 'light');
-}
-
-function dark() {
-	document.documentElement.setAttribute('theme', 'dark');
-}
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-	dark();
-} else {
-	light();
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-	if (event.matches) {
-		dark();
-	} else {
-		light();
-	}
-});
-
-
 
 
 
