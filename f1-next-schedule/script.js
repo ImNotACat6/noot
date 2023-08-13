3a9de2594ef3bce4899a08f4596e4fe16c0ff389
ImNotACat6 <40551723+ImNotACat6@users.noreply.github.com>
2023-08-13 11:14:42 -0400

    Initial f1-next-schedule commit

---
+++ f1-next-schedule/script.js
@@ -0,0 +1,21 @@
+function light() {
+	document.documentElement.setAttribute('theme', 'light');
+}
+
+function dark() {
+	document.documentElement.setAttribute('theme', 'dark');
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	dark();
+} else {
+	light();
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+	if (event.matches) {
+		dark();
+	} else {
+		light();
+	}
+});
